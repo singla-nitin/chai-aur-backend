4a94fc3937e9b2b6d84db09915329b5607b0aec1
Nitin singla <nitinsingla702@gmail.com>
2024-01-01 22:34:00 +0530

    cmmonjs to modulejs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
-const express = require("express")
+import express from 'express';
 const app = express()
-const port = 4000
+
 
 app.get( "/", (req, res) => {
     res.send("<h1> Welcome to the Home page.</h1>")
